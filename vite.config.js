1487b8c55439b23ea68d0a32e1eff744a186db8a
Wonuola <wonuolalonge@gmail.com>
2023-03-19 20:35:17 +0100

    bug-fix

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import { popper } from '@popperjs/core'
 
 // https://vitejs.dev/config/
+const path = require('path')
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['bootstrap']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  // root: path.resolve()
   build: {
     rollupOptions: {
       external:['/IKJF2031.JPG', '@popperjs/core']
