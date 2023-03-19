810382edf80e7029fb478f15cee4a4b689357d00
Wonuola <wonuolalonge@gmail.com>
2023-03-19 19:51:04 +0100

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external:['/IKJF2031.JPG', 'popperjs/core']
+      external:['/IKJF2031.JPG', '@popperjs/core']
     }
   }
 })
