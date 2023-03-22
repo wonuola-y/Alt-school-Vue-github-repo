af9d94138c6468983766cc106bb685f289744981
Wonuola <wonuolalonge@gmail.com>
2023-03-22 15:18:44 +0100

    bug-fix

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Repos from '../views/Repositories.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
